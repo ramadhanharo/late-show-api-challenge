0daf2d6ba2315f6fb6869f708f840b811069baba
ramadhanharo <ramadhan.haro@moringaschool.com>
2025-06-24 11:49:47 +0300

    updated config.py

--- server/config.py
+++ server/config.py
@@ -6,4 +6,4 @@ class Config:
         "postgresql://ramadhanharo:rama123@localhost:5432/late_show_db"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")  # Change this to something more secure!
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "super-secret-key")  
